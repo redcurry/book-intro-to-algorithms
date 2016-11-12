faec5ec26b1312fef44dd4340043aff60264b0bc
Carlos Anderson <carlosjanderson@gmail.com>
2016-11-12 16:45:15 -0500

    Fix binary search

--- ch2/binary-search.py
+++ ch2/binary-search.py
@@ -24,13 +24,16 @@ def binary_search(a, v):
     return binary_search_rec(a, v, 0, len(a) - 1)
 
 def binary_search_rec(a, v, low, high):
+    if low > high:
+        return -1
+
     if low == high:
         if v == a[low]:
             return low
         else:
             return -1
 
-    mid = int(math.floor((low + high) / 2.0))
+    mid = (low + high) / 2
 
     if v < a[mid]:
         return binary_search_rec(a, v, low, mid - 1)
